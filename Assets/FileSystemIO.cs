3e2251168e5ae1a0e1f2ee9961049d2ad3a95135
Tchou <Tchou2905@gmail.com>
2020-08-18 20:17:51 +0200

    Initial commit

---
+++ Assets/FileSystemIO.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace TchouSaveSystem{
+
+	public static class FileSystemIO {
+
+
+		/// <summary>
+		/// This fonction takes an object, serialize it and write it into a file
+		/// </summary>
+		/// <param name="fileName">Name of the file you want to write to without the extension</param>
+		/// <param name="url">Path to your destination (example : "folder1/"</param>
+		/// <param name="format">The extension of the file (example : ".bin")</param>
+		/// <param name="data">A serializable object</param>
+		/// <returns>Return true if the operation was succesful else, it throws an error</returns>
+		public static bool SaveBinaryFile(string fileName, string path, string format, object data) {
+
+			string destination = Consts.DataPath + "/" + path + fileName + format;
+
+			Debug.Log(destination);
+
+			FileStream file;
+			if (File.Exists(destination)) file = File.OpenWrite(destination);
+			else file = File.Create(destination);
+			BinaryFormatter bf = new BinaryFormatter();
+
+			SurrogateSelector ss = new SurrogateSelector();
+			CustomSerializer v3ss = new CustomSerializer();
+			ss.AddSurrogate(typeof(Vector3), new StreamingContext(StreamingContextStates.All), v3ss);
+			bf.SurrogateSelector = ss;
+
+			try {
+				bf.Serialize(file, data);
+				return true;
+			}
+			catch (SerializationException e) {
+				Debug.LogError("Failed to serialize. Reason: " + e.Message);
+				return false;
+			}
+			finally {
+				file.Close();
+			}
+		}
+
+		public static object LoadBinaryFile(string fileName, string path, string format, out object data) {
+			string destination = Consts.DataPath + "/" + path + fileName + format;
+			data = null;
+			FileStream file;
+
+			if (File.Exists(destination)) file = File.OpenRead(destination);
+			else {
+				Debug.LogError("File not found");
+				return null;
+			}
+
+			BinaryFormatter bf = new BinaryFormatter();
+			SurrogateSelector ss = new SurrogateSelector();
+			CustomSerializer v3ss = new CustomSerializer();
+			ss.AddSurrogate(typeof(Vector3), new StreamingContext(StreamingContextStates.All), v3ss);
+			bf.SurrogateSelector = ss;
+
+
+			try {
+				data = (object)bf.Deserialize(file);
+				return data;
+			}
+			catch (SerializationException e) {
+				Debug.LogError("Failed to serialize. Reason: " + e.Message);
+				return null;
+			}
+			finally {
+				file.Close();
+			}
+		}
+
+	}
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TestScript : MonoBehaviour
+{
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		var TestObject = new TestObject();
+		TestObject.name = "TestName";
+		TestObject.intTest = 5;
+		TestObject.vector3Test = new Vector3(10.6f, 5, 7f);
+		TestObject.floatTest = 895.454f;
+		TchouSaveSystem.FileSystemIO.SaveBinaryFile("test", "", ".bin", TestObject);
+		object obj;
+		TchouSaveSystem.FileSystemIO.LoadBinaryFile("test", "", ".bin", out obj);
+
+		var TestObject2 = (TestObject)obj;
+		Debug.Log(TestObject2.name);
+		Debug.Log(TestObject2.intTest);
+		Debug.Log(TestObject2.vector3Test);
+		Debug.Log(TestObject2.floatTest);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+class TestObject {
+	public string name;
+	public int intTest;
+	public Vector3 vector3Test;
+	public float floatTest;
+}
